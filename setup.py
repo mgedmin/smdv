0a9b96c9a7b0af35fbb78f7c3854dbcb1c4c7a8e
Gedminas, Marius <marius.gedminas@draeger.com>
2026-07-15 16:48:43 +0300

    Parse the AST instead of importing smdv in setup.py
    This way we don't need to setup_requires=["flask", "websockets"].
    
    Fixes `uv tool install -e .` and other use cases that do builds by
    running setup.py in a fresh virtualenv.
    
    The use of match statement means we require Python >= 3.10.  I could've
    used regexes instead, but I wanted to play with the ast module, mostly
    because of ast.get_docstring().

--- setup.py
+++ setup.py
@@ -1,21 +1,34 @@
-import smdv
+import ast
+import tokenize
 from setuptools import setup
+
+with tokenize.open("smdv.py") as f:
+    module = ast.parse(f.read(), filename=f.name)
+
+metadata = {}
+for stmt in module.body:
+    match stmt:
+        case ast.Assign(targets=[ast.Name(id=name)], value=ast.Constant()):
+            metadata[name] = ast.literal_eval(stmt.value)
+author = metadata["__author__"]
+version = metadata["__version__"]
+description = ast.get_docstring(module)
 
 with open("readme.md", "r") as f:
     long_description = f.read()
 
 setup(
     name="smdv",
-    version=smdv.__version__,
-    author=smdv.__author__,
+    version=version,
+    author=author,
     author_email="floris.laporte@gmail.com",
-    description=smdv.__doc__,
+    description=description,
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/flaport/smdv",
     py_modules=["smdv"],
     entry_points={"console_scripts": ["smdv = smdv:main"]},
-    python_requires=">=3.6",
+    python_requires=">=3.10",
     install_requires=["flask", "websockets"],
     classifiers=[
         "Topic :: Utilities",
